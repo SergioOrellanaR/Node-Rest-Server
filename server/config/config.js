8e283a1608c1adefaaba70e3e1eb9150a76a7d8d
Sergio Orellana <orellana123@gmail.com>
2020-03-22 04:40:35 -0300

    Sec changes

--- server/config/config.js
+++ server/config/config.js
@@ -36,17 +36,3 @@ let urlDB;
 
 
 
-if (process.env.NODE_ENV === 'dev')
-{
-    urlDB = `mongodb://localhost:${mongooseDbport}/cafe`;
-}
-else
-{
-    const user = 'serorellanar';
-    const pass = 'lnjzFzmWJ4KjGh08';
-    const cluster = 'orellanacluster-599jo.mongodb.net/cafe';
-
-    urlDB = `mongodb+srv://${user}:${pass}@${cluster}`;
-}
-
-process.env.URLDB = urlDB;
